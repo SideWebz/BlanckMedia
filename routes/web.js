fcb3886b6b6be014c0b8f5e438f0074f32203443
SideWebz <webzside@gmail.com>
2025-10-23 22:29:55 +0200

    Under Construction

--- routes/web.js
+++ routes/web.js
@@ -1,13 +1,7 @@
 import express from 'express';
 import { home } from '../controllers/homeController.js';
-import { projects } from '../controllers/projectsController.js';
-import { about } from '../controllers/aboutController.js';
-import { contact } from '../controllers/contactController.js';
 
 const router = express.Router();
 router.get('/', home);
-router.get('/projects', projects);
-router.get('/about', about);
-router.get('/contact', contact);
 
 export default router;
